4115f7e66872cfbae8fbf67fb8245f24f21a1d9f
domin <domin@Andrys-PC>
2022-02-28 00:44:53 -0500

    lab work done and homework part 1 commit

--- Lab04_HW/src/main/java/com/mycompany/lab04_hw/Task01.java
+++ Lab04_HW/src/main/java/com/mycompany/lab04_hw/Task01.java
@@ -2,12 +2,15 @@
 package com.mycompany.lab04_hw;
 
 /**
- * what is the output of this code? ... .... .... .... Blastoff. 
+ * what is the output of this code? 
+ * Answer: ... .... .... .... Blastoff. 
  * Depending on the int entered for num, the program will print ... until the countdown reaches 0, then it will print blastoff. 
  *
  *  - modify it so that it prints only the even numbers. 
  *
  *  - what is the time complexity of this algorithm and why?
+ * Answer: O(n)
+ * The algorithm is going to recursively reduce by 1, which is a constant. So the algorithms time complexity would mostly depend on int num or n.  
  * @author Andry
  */
 public class Task01 {
--- Lab04_HW/src/main/java/com/mycompany/lab04_hw/Task03.java
+++ Lab04_HW/src/main/java/com/mycompany/lab04_hw/Task03.java
@@ -3,6 +3,9 @@ package com.mycompany.lab04_hw;
 
 /**
  * what is the time complexity of this algorithm and why?
+ * Answer : O(2^n) exponential time complexity. 
+ * The algorithm will recursively call itself twice until n either equals 0 or 1.
+ * Because its breaking each call down to two more calls, the time complexity will grow exponentially.
  * 
  * @author Andry
  */
@@ -22,7 +25,7 @@ public class Task03 {
 
        else
 
-             return fib(n – 1) + fib(n – 2);
+             return fib(n – 1) + fib(n – 2); 
 
 }
     
